107f9d37f4ed75c9177e4f30deef1393e7d996d8
John McBride <john@opensauced.pizza>
2024-06-20 14:47:54 -0600

    feat: Fuzzy search all repos (#890)
    Includes adding the "pg_trgm" extension and a GIN index on the repo table's
    "full_name" column.
    
    Signed-off-by: John McBride <john@opensauced.pizza>

--- src/repo/dtos/repo-search-options.dto.ts
+++ src/repo/dtos/repo-search-options.dto.ts
@@ -1,9 +1,10 @@
 import { ApiProperty, ApiPropertyOptional } from "@nestjs/swagger";
-import { IsEnum, IsIn, IsInt, IsOptional, IsString } from "class-validator";
+import { IsEnum, IsIn, IsInt, IsOptional, IsString, Max, Min } from "class-validator";
 import { Type } from "class-transformer";
 import { InsightFilterFieldsEnum } from "../../insight/dtos/insight-options.dto";
 
-import { RepoPageOptionsDto } from "./repo-page-options.dto";
+import { OrderDirectionEnum } from "../../common/constants/order-direction.constant";
+import { RepoOrderFieldsEnum, RepoPageOptionsDto } from "./repo-page-options.dto";
 
 export class RepoSearchOptionsDto extends RepoPageOptionsDto {
   @ApiPropertyOptional({
@@ -74,7 +75,7 @@ export class RepoRangeOptionsDto {
   readonly prev_days_start_date?: number = 0;
 }
 
-export class RepoFuzzySearchOptionsDto extends RepoPageOptionsDto {
+export class RepoFuzzySearchOptionsDto {
   @ApiProperty()
   @IsString()
   readonly fuzzy_repo_name: string;
@@ -86,4 +87,46 @@ export class RepoFuzzySearchOptionsDto extends RepoPageOptionsDto {
   @IsString()
   @IsOptional()
   readonly topic?: string;
+
+  @ApiPropertyOptional({
+    enum: RepoOrderFieldsEnum,
+    enumName: "RepoOrderFieldsEnum",
+    default: RepoOrderFieldsEnum.stars,
+  })
+  @IsEnum(RepoOrderFieldsEnum)
+  @IsOptional()
+  readonly orderBy?: RepoOrderFieldsEnum = RepoOrderFieldsEnum.stars;
+
+  @ApiPropertyOptional({
+    minimum: 1,
+    default: 1,
+    type: "integer",
+  })
+  @Type(() => Number)
+  @IsInt()
+  @Min(1)
+  @IsOptional()
+  readonly page?: number = 1;
+
+  @ApiPropertyOptional({
+    minimum: 1,
+    maximum: 1000,
+    default: 10,
+    type: "integer",
+  })
+  @Type(() => Number)
+  @IsInt()
+  @Min(1)
+  @Max(10000)
+  @IsOptional()
+  readonly limit?: number = 50;
+
+  @ApiPropertyOptional({ enum: OrderDirectionEnum, enumName: "OrderDirectionEnum", default: OrderDirectionEnum.DESC })
+  @IsEnum(OrderDirectionEnum)
+  @IsOptional()
+  readonly orderDirection?: OrderDirectionEnum = OrderDirectionEnum.DESC;
+
+  get skip(): number {
+    return ((this.page ?? 1) - 1) * (this.limit ?? 50);
+  }
 }
--- src/repo/repo.service.ts
+++ src/repo/repo.service.ts
@@ -271,19 +271,11 @@ export class RepoService {
 
   async fastFuzzyFind(pageOptionsDto: RepoFuzzySearchOptionsDto): Promise<PageDto<DbRepo>> {
     const orderField = pageOptionsDto.orderBy ?? RepoOrderFieldsEnum.pushed_at;
-    const startDate = GetPrevDateISOString(pageOptionsDto.prev_days_start_date);
-    const range = pageOptionsDto.range!;
-
     const queryBuilder = this.baseFilterQueryBuilder()
       .withDeleted()
       .addSelect("repos.deleted_at")
       .where(`full_name ILIKE :fuzzy_search_param`, {
         fuzzy_search_param: `%${pageOptionsDto.fuzzy_repo_name}%`,
-      })
-      .andWhere(`:start_date::TIMESTAMP >= "repos"."updated_at"`, { start_date: startDate })
-      .andWhere(`:start_date::TIMESTAMP - :range_interval::INTERVAL <= "repos"."updated_at"`, {
-        start_date: startDate,
-        range_interval: `${range} days`,
       })
       .orderBy(`"repos"."${orderField}"`, OrderDirectionEnum.DESC)
       .offset(pageOptionsDto.skip)
